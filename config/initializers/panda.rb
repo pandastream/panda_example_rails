d9518afa3403da319cac05606c931e5663b62e3b
vivien <vivien@vivien-new-bamboo-mpb.local>
2010-03-05 13:02:22 +0000

    Added host and port

--- config/initializers/panda.rb
+++ config/initializers/panda.rb
@@ -4,7 +4,9 @@ if ['production'].include?(RAILS_ENV)
   PANDA_CONFIG = {
      "access_key"  =>  ENV['PANDASTREAM_ACCESS_KEY'],
      "secret_key"  =>  ENV['PANDASTREAM_SECRET_KEY'],
-     "api_port"    =>  ENV['PANDASTREAM_CLOUD_ID']
+     "cloud_id"    =>  ENV['PANDASTREAM_CLOUD_ID'],
+     "api_host"    =>  ENV['PANDASTREAM_API_HOST'],
+     "api_port"    =>  ENV['PANDASTREAM_API_PORT']
    }
 else
   PANDA_CONFIG = YAML::load(File.open(File.join(File.dirname(__FILE__),'..', 'panda.yml')))[RAILS_ENV]
