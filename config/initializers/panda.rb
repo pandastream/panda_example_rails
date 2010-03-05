7c71d55ad21cf1fc5eb0e3f09364a7c9018856b5
vivien <vivien@vivien-new-bamboo-mpb.local>
2010-03-05 14:31:17 +0000

    Removed heroku example. now in a different branch

--- config/initializers/panda.rb
+++ config/initializers/panda.rb
@@ -1,15 +1,2 @@
-if ['production'].include?(RAILS_ENV)
-  # if our application is deployed to heroku
-  # we fetch variables from you environment
-  PANDA_CONFIG = {
-     "access_key"  =>  ENV['PANDASTREAM_ACCESS_KEY'],
-     "secret_key"  =>  ENV['PANDASTREAM_SECRET_KEY'],
-     "cloud_id"    =>  ENV['PANDASTREAM_CLOUD_ID'],
-     "api_host"    =>  ENV['PANDASTREAM_API_HOST'],
-     "api_port"    =>  ENV['PANDASTREAM_API_PORT']
-   }
-else
-  PANDA_CONFIG = YAML::load(File.open(File.join(File.dirname(__FILE__),'..', 'panda.yml')))[RAILS_ENV]
-end
-
+PANDA_CONFIG = YAML::load(File.open(File.join(File.dirname(__FILE__),'..', 'panda.yml')))[RAILS_ENV]
 PANDA = Panda.new(PANDA_CONFIG)
