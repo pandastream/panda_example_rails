40c762a379ed58d5204f7f8ef8c54fa2ffb81af8
vivien <vivien@vivien-new-bamboo-mpb.local>
2010-04-13 17:14:57 +0100

    Fixed panda url

--- config/initializers/panda.rb
+++ config/initializers/panda.rb
@@ -1,13 +1,7 @@
 if ['production'].include?(RAILS_ENV)
   # if our application is deployed to heroku
   # we fetch variables from you environment
-  PANDA_CONFIG = {
-     "access_key"  =>  ENV['PANDASTREAM_ACCESS_KEY'],
-     "secret_key"  =>  ENV['PANDASTREAM_SECRET_KEY'],
-     "cloud_id"    =>  ENV['PANDASTREAM_CLOUD_ID'],
-     "api_host"    =>  ENV['PANDASTREAM_API_HOST'],
-     "api_port"    =>  ENV['PANDASTREAM_API_PORT']
-   }
+  PANDA_CONFIG = ENV['PANDASTREAM_URL']
 else
   PANDA_CONFIG = YAML::load(File.open(File.join(File.dirname(__FILE__),'..', 'panda.yml')))[RAILS_ENV]
 end
