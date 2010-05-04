4f054e9448120c5a001623872862bf11c83c406e
Pablo Brasero <pablo@new-bamboo.co.uk>
2010-05-04 15:56:01 +0100

    Made example similar to those in PHP and Python

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,22 @@
+require 'ostruct'
+require 'pp'
+
 class VideosController < ApplicationController
   def show
     s3_bucket_url = "http://#{S3_CONFIG['bucket']}.s3.amazonaws.com"
 
     @video = Video.find(params[:id])
-    @video_data =  JSON.parse(PANDA.get("/videos/#{@video.panda_video_id}.json"))
-    # Find the url of the video from the first profile
-    @encoding_data = JSON.parse(PANDA.get("/videos/#{@video.panda_video_id}/encodings.json")).first
-    @video_url = s3_bucket_url + @encoding_data['id'] + @encoding_data['extname']
-    @screenshot_url = s3_bucket_url + @encoding_data['id'] + "_4.jpg"
+    encodings = JSON.parse(PANDA.get("/videos/#{@video.panda_video_id}/encodings.json"))
+    encoding_data = encodings.find{|e| e['status'] == 'success' && e['extname'] == '.mp4' }
+    if encoding_data
+      @encoding = OpenStruct.new(encoding_data)
+      @encoding.panda_encoding_id = encoding_data['id']
+      @encoding.url = s3_bucket_url + '/' + @encoding.panda_encoding_id + @encoding.extname
+      @encoding.screenshot_url = s3_bucket_url + '/' + @encoding.panda_encoding_id + "_4.jpg"
+    end
+    
+    @pretty_printed_encodings = ''
+    PP.pp(encodings, @pretty_printed_encodings)
   end
   
   def new
