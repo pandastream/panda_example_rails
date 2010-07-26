40c3af715bd6028766db4b188ded1b29370eac37
Gwyn Morfey <gwyn@Unknown-00-23-6c-89-83-be.config>
2010-07-26 10:54:30 +0100

    Mucking around with a new way of doing things. Works?

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,24 +1,7 @@
-require 'ostruct'
-require 'pp'
-
+#No reference to Panda at all here - it's all handled in the Video model.
 class VideosController < ApplicationController
   def show
-    s3_bucket_url = "http://#{S3_CONFIG['bucket']}.s3.amazonaws.com"
-
     @video = Video.find(params[:id])
-    encodings = PANDA.get("/videos/#{@video.panda_video_id}/encodings.json")
-    encoding_data = encodings.find{|e| e['status'] == 'success' && e['extname'] == '.mp4' }
-    if encoding_data
-      @encoding = OpenStruct.new(encoding_data)
-      @encoding.panda_encoding_id = encoding_data['id']
-      @encoding.url = s3_bucket_url + '/' + @encoding.panda_encoding_id + @encoding.extname
-      @encoding.screenshot_url = s3_bucket_url + '/' + @encoding.panda_encoding_id + "_4.jpg"
-    end
-    
-    @pretty_printed_encodings = ''
-    PP.pp(encodings, @pretty_printed_encodings)
-    
-    render 'show_flash' if params[:type] == "flash"
   end
   
   def new
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,12 @@
 class Video < ActiveRecord::Base
+  #Panda also provides a Panda::Video, but we wrap it in our own ActiveRecord
+  #class in order to get local storage, and so that we can add our own
+  #associations and metadata.
+  
+  validates_presence_of :panda_video_id
+  
+  #Provide access to the wrapped object
+  def panda
+    Panda::Video.id(panda_video_id)
+  end
 end
