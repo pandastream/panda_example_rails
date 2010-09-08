e3927d44bfc66512c408b6204c7cdbda8e45e884
Pablo Brasero <pablo@new-bamboo.co.uk>
2010-09-08 14:33:10 +0100

    The h264 profile may go by different names

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,7 +2,7 @@ class VideosController < ApplicationController
   def show
     @video = Video.find(params[:id])
     @panda_video = @video.panda_video
-    @h264_encoding = @panda_video.encodings.find_by_profile_name("h264") 
+    @h264_encoding = find_h264_encoding(@panda_video)
   end
   
   def new
@@ -13,4 +13,15 @@ class VideosController < ApplicationController
     @video = Video.create!(params[:video])
     redirect_to :action => :show, :id => @video.id
   end
+
+
+  private
+  
+  def find_h264_encoding(video)
+    name = %w{h264.hi h264 h264.lo}.find do |name|
+      video.encodings.find_by_profile_name(name)
+    end
+    video.encodings.find_by_profile_name(name)
+  end
+
 end
