becc15918f7c99ef7af26ef17e239e65e571c357
Vivien Schilis <vivien.schilis@gmail.com>
2012-12-11 14:44:12 +0000

    New How To using the beta uploader 2.0.0-pre5 and the resumable upload api

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,16 +1,33 @@
 class VideosController < ApplicationController
+  include Rails.application.routes.url_helpers
+
   def show
     @video = Video.find(params[:id])
     @original_video = @video.panda_video
     @h264_encoding = @original_video.encodings['h264']
   end
-  
-  def new
+
+  def simple
     @video = Video.new
+    render 'simple'
   end
-  
+
+  def advanced
+    render 'advanced'
+  end
+
   def create
     @video = Video.create!(params[:video])
     redirect_to :action => :show, :id => @video.id
   end
+
+  def postprocess
+    video_params = JSON.parse(params["upload_response"])
+    @video = Video.new(:panda_video_id => video_params["id"], :title => "Title-to-be-edited-later")
+
+    if @video.save
+      render :json => {:play_url => url_for(@video), :id => @video_id}
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 PandaExampleRails::Application.routes.draw do
   resources :videos
-  root :to => "videos#new"
+
+  match "/panda/authorize_upload", :to => "panda#authorize_upload"
+  match "/panda/authorize_upload_postprocess", :to => "panda#authorize_upload_postprocess"
+
+  match "/simple", :to => "videos#simple"
+
+  match "/advanced", :to => "videos#advanced"
+
+  match "/videos/postprocess", :to => "videos#postprocess"
+
+  root :to => "videos#simple"
+
 end
