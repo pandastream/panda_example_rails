ba812f03e9915ff26c377200fae505ecb0f2d99f
Vivien Schilis <vivien.schilis@gmail.com>
2013-07-08 14:43:19 +0100

    Updated example with csrf handling

--- app/controllers/panda_controller.rb
+++ app/controllers/panda_controller.rb
@@ -1,23 +1,30 @@
 class PandaController < ApplicationController
+  def verify_authenticity_token
+    handle_unverified_request unless (form_authenticity_token == upload_payload['csrf'])
+  end
+
+  def upload_payload
+    @upload_payload ||= JSON.parse(params['payload'])
+  end
+
   def authorize_upload
-    payload = JSON.parse(params['payload'])
-    new_upload = Panda.post('/videos/upload.json', {
-      file_name: payload['filename'],
-      file_size: payload['filesize'],
+    upload = Panda.post('/videos/upload.json', {
+      file_name: upload_payload['filename'],
+      file_size: upload_payload['filesize'],
       profiles: "h264",
     })
 
-    render :json => {:upload_url => new_upload['location']}
+    render :json => {:upload_url => upload['location']}
   end
 
   def authorize_upload_postprocess
-    payload = JSON.parse(params['payload'])
-    new_upload = Panda.post('/videos/upload.json', {
-      file_name: payload['filename'],
-      file_size: payload['filesize'],
+
+    upload = Panda.post('/videos/upload.json', {
+      file_name: upload_payload['filename'],
+      file_size: upload_payload['filesize'],
       profiles: "h264",
     })
 
-    render :json => {:upload_url => new_upload['location'], :postprocess_url => "/videos/postprocess"}
+    render :json => {:upload_url => upload['location'], :postprocess_url => "/videos/postprocess"}
   end
 end
